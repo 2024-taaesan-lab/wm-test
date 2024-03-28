44071e4f1924a093b930c1b51ce873769512f2c7
Taaesan <taaesan@gmail.com>
2024-03-28 20:34:37 +0700

    Grafana + Prometheus

--- shortest-path-app/src/main/java/com/wavemoney/assessment/controller/ShortestPathController.java
+++ shortest-path-app/src/main/java/com/wavemoney/assessment/controller/ShortestPathController.java
@@ -1,6 +1,7 @@
 package com.wavemoney.assessment.controller;
 
-
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import com.wavemoney.assessment.dto.GridRequest;
 import com.wavemoney.assessment.model.Path;
 import com.wavemoney.assessment.service.PathService;
@@ -10,24 +11,39 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.PresentationDirection;
+import java.util.concurrent.TimeUnit;
+
 /**
  * The ShortestPathController class handles requests related to finding the shortest path.
  * It maps HTTP requests to corresponding methods and delegates the processing to the PathService.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class ShortestPathController {
 
-    private final PathService pathService; // Service for finding the shortest path
-
-    /**
-     * Constructs a ShortestPathController with the specified PathService implementation.
-     *
-     * @param pathService The PathService implementation used for finding the shortest path.
-     */
     @Autowired
-    public ShortestPathController(@Qualifier("bfsPathServiceImpl") PathService pathService) {
-        this.pathService = pathService;
+    @Qualifier("bfsPathServiceImpl")
+    private PathService pathService; // Service for finding the shortest path
+
+    private MeterRegistry registry;
+    @Autowired
+    public ShortestPathController(MeterRegistry meterRegistry) {
+
+        this.registry = meterRegistry;
+
+        Timer timer = registry.timer("Time for operation");
+        timer.record(() -> {
+            int sum = 0;
+            for (int i = 0; i <= 1000; i++) {
+                sum += i;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     /**
@@ -38,12 +54,15 @@ public class ShortestPathController {
      */
     @PostMapping(value="/shortest-path")
     public ResponseEntity<Path> findShortestPath(@RequestBody GridRequest gridRequest) {
+        Timer.Sample sample = Timer.start(registry);
+
         int[][] grid = gridRequest.getGrid();
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return ResponseEntity.badRequest().build();
         }
 
         Path path = pathService.findShortestPath(grid);
+        sample.stop(registry.timer("my.timer", "response", "OK"));
         return new ResponseEntity<>(path, HttpStatus.OK);
     }
 }
